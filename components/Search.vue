da7bd290283d851e735173580f2c49d91d601f73
rutko <t.222cm@gmail.com>
2022-05-08 16:53:10 +0900

    first commit

---
+++ components/Search.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="area"
+            label="エリア・現在地"
+            prepend-inner-icon="mdi-map-marker"
+            outlined
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="party"
+            label="犬種"
+            prepend-inner-icon="mdi-dog"
+            outlined
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="keyword"
+            label="キーワード"
+            prepend-inner-icon="mdi-pencil"
+            outlined
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-checkbox
+            v-model="pickUp"
+            label="送迎あり"
+          ></v-checkbox>
+        </v-col>
+        <v-col>
+          <v-checkbox
+            v-model="pickUp"
+            label="ライブ配信あり"
+          ></v-checkbox>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card class="logo py-4 d-flex justify-center">
+      <Search />
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
