d5730272d1776219102d0d536639b3c0c3044a9f
rutko <t.222cm@gmail.com>
2022-05-14 23:52:18 +0900

    👍グループページを追加

--- components/RecommendItems.vue
+++ components/RecommendItems.vue
@@ -1,12 +1,19 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col>
-        <v-card outlined>
+      <v-col v-for="item in items" :key="item.id">
+        <v-card elevation="0" outlined max-width="160px" v-on:click="onClick(item.id)">
           <v-img
-            height="250"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-          ></v-img>
+            max-height="160px"
+            src="img/anifareLogo.png"
+          />
+
+          <v-card-title class="pb-0">
+            <span class="item-title">{{ item.name }}</span>
+          </v-card-title>
+          <v-card-text class="text-subtitle-2 pt-0">
+            {{ item.description}}
+          </v-card-text>
 
         </v-card>
       </v-col>
@@ -14,3 +21,20 @@
 
   </v-container>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'anifare', description:'全国300以上の動物病院と提携', url: 'https://anifare.jp/' },
+      ]
+    }
+  },
+  methods: {
+    onClick(id){
+      this.$router.push({ path: `/group/${id}` })
+    }
+  }
+}
+</script>
---
+++ pages/group/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-toolbar>
+      <v-toolbar-title>
+        <h4></h4>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn color="primary" rounded depressed>
+        公式サイトへ
+      </v-btn>
+    </v-toolbar>
+  </v-container>
+</template>
